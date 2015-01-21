69e55e5fdaf2dc5b246f463dd92a8035faa9f46f
Alexander Stekelenburg <superaxander@users.noreply.github.com>
2015-01-21 09:06:28 +0100

    Update luaoop.lua

--- luaoop.lua
+++ luaoop.lua
@@ -2,6 +2,17 @@
 LuaOOP Library
 By: Alexander S.
 
-This file may be used for comercial and personal use. As long as credit is given and a reference to the http://raw.github.com/superaxander/LuaOOP/master/luaoop.lua
+This file may be used for comercial and personal use. As long as credit is given and a reference to the https://github.com/superaxander/LuaOOP/raw/master/luaoop.lua
 
 ]]--
+
+function createObject(...)
+    local newObject = {}
+    for i, v in pairs({...})
+        if isEven(i) then
+            table.insert(newObject, x, v)
+        else
+            local x = v
+        end
+    end
+end
