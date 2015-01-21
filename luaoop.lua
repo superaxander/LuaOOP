0b50ca2cbb2de81256294aa2cf1cbbcacab30321
Alexander Stekelenburg <superaxander@users.noreply.github.com>
2015-01-21 08:57:19 +0100

    Create luaoop.lua

---
+++ luaoop.lua
@@ -0,0 +1,7 @@
+--[[
+LuaOOP Library
+By: Alexander S.
+
+This file may be used for comercial and personal use. As long as credit is given and a reference to the http://raw.github.com/superaxander/LuaOOP/master/luaoop.lua
+
+]]--
